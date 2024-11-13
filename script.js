1dec69acecee9171ca433de18c50e216fb6e87de
anandhuraj k r <anandhurajloop1311@gmail.com>
2024-11-13 11:27:40 +0400

    javascript assignment 3 - dom selectors

---
+++ script.js
@@ -0,0 +1,20 @@
+let mheading = document.getElementById("heading")
+console.log(mheading , "heading");
+ 
+let div = document.getElementsByClassName("container")
+console.log(div, "container")
+
+let p = document.getElementsByTagName("p")
+console.log(p, "ptag")
+
+let button = document.querySelector(".btn")
+console.log(button , "button")
+
+let section = document.querySelector (".section")
+console.log(section , "section element")
+
+let allbuttons = document.querySelectorAll (".btn")
+console.log(allbuttons , "btn")
+
+let allptags = document.querySelectorAll("p")
+console.log(allptags , "p tags")
